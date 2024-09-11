ce1eae1a8501f935dcd92421d79e237ed647f4c2
Bruno Gomes <brunog.damascena@gmail.com>
2024-09-11 15:53:25 -0300

    iniciando projeto

---
+++ src/test/java/com/brunocompany/kafkatests/KafkaTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunocompany.kafkatests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
